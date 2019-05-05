372f300f7d46e61bb87684b183672b0c7e0e220d
Smirk <smirk.cao@gmail.com>
2019-05-05 14:54:17 +0800

    ref downloader

---
+++ ref_downloader.sh
@@ -0,0 +1,8 @@
+mkdir refs
+cd refs
+
+echo "Welsh95"
+wget "https://www.cs.unc.edu/~welch/media/pdf/kalman_intro.pdf" -O Welsh95.pdf
+
+echo "Thrun05"
+wget "https://docs.ufpr.br/~danielsantos/ProbabilisticRobotics.pdf" -O Thrun05.pdf
